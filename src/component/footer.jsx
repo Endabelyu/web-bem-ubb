32e52c4cf6405a7ec0d9583fc06d34ba3c144943
endabelyu <endabelyuproject@gmail.com>
2022-07-24 14:33:10 +0700

    add a new p on footer

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 const footer = () => {
-  const urlWebBEM = 'https://bem-ubb.netlify.app/';
+  const BEM = {
+    url: 'https://bem-ubb.netlify.app/',
+    target: '_blank',
+  };
+
   return (
     <div className="p-10 border-t-2 border-gray-400">
       <div className="flex flex-col justify-center ">
-        <p className=" text-center text-md">Built With love by BEM KM UBB</p>
         <p className=" text-center text-md">
-          C All right Reversed |
-          <a href={urlWebBEM} className="ml-2">
-            2022
+          Made With love by
+          <a href={BEM.url} target={BEM.target} className="ml-2">
+            BEM KM UBB
           </a>
         </p>
+        <p className=" text-center text-md">C All right Reversed | 2022</p>
       </div>
     </div>
   );
