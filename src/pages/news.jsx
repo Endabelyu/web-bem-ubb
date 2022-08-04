a659ea8769181405c7e4fdb7e83ba4aca00370eb
endabelyu <endabelyuproject@gmail.com>
2022-08-04 09:17:45 +0700

    add a example on news pages

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Navbar from '../component/navbar';
+import Footer from '../component/footer';
 
 const news = () => {
   return (
     <div>
       <Navbar />
-      <div className="mt-48">
-        <h1>news </h1>
+      <div className="mt-48 my-32 mx-10">
+        <ul>
+          <li>
+            <iframe
+              src="https://news.detik.com/berita/d-6213897/mahfud-sebut-kasus-brigadir-j-beda-dengan-kriminal-biasa-ini-alasannya"
+              className="w-full h-[300px] border-2 border-gray-300"
+              title="berita terbaru"
+            />
+          </li>
+        </ul>
       </div>
+      <Footer />
     </div>
   );
 };
